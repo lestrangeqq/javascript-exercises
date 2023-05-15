6468aa527e341341d5c985840e6fe136546ff7d6
lestrangeqq <lestrangeqq@gmail.com>
2023-05-15 20:56:39 +0300

    Ex. 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+  let multi = '';
+  for (i=0; i<number; i++) {
+    multi += string;
+  }
+  return multi
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
