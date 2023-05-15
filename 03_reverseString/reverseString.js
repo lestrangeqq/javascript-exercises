0af7b68e7d410902eacf761e9df1ee6728bf7ed6
lestrangeqq <lestrangeqq@gmail.com>
2023-05-15 21:11:36 +0300

    Ex.3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let arr = str.split('')
+  let arrLen = arr.length;
+  let reverse = ''
+  for (i=1; i!=arrLen+1; i++) {
+    reverse += arr[arrLen-i];
+  }
+  return reverse
 };
+
+reverseString('hello')
 
 // Do not edit below this line
 module.exports = reverseString;
